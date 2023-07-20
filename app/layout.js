836f061618acf10ea06fd411e9aad8870222bafe
jesjo001 <automationlounge@gmail.com>
2023-07-20 03:12:15 +0100

    version1 styled app with next and implemented theme switcher

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import GlobalThemeWrapper from './lib/GlobalThemeWrapper'
+import StyledComponentsRegistry from './lib/register'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +11,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <StyledComponentsRegistry>
+          <GlobalThemeWrapper>
+          {children}
+          </GlobalThemeWrapper>
+        </StyledComponentsRegistry>
+      </body>
     </html>
   )
 }
